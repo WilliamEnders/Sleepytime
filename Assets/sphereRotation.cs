f6e30ae6aee516a9c1d518f4b2b171ea57123de8
William Enders <endew231@newschool.edu>
2016-02-25 17:37:26 -0500

    third person controls

---
+++ Assets/sphereRotation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sphereRotation : MonoBehaviour
+{
+	void Update(){
+		transform.parent.Rotate (0,Input.GetAxis("Horizontal"),0);
+	}
+}
