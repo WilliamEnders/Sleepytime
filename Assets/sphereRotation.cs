446487f20277c3175bf80fa69643b857ded5fcb4
William Enders <endew231@newschool.edu>
2016-02-25 18:05:58 -0500

    simple vr room with multiplayer set up

--- Assets/sphereRotation.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class sphereRotation : MonoBehaviour
-{
-	void Update(){
-		transform.parent.Rotate (0,Input.GetAxis("Horizontal"),0);
-	}
-}
