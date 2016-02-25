b0ceaf2d5996b6bafd6f6a2bc60769fc3ed8e66f
William Enders <endew231@newschool.edu>
2016-02-24 20:38:13 -0500

    four player split screen + vr

---
+++ Assets/stopMirror.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class stopMirror : MonoBehaviour {
+	IEnumerator Start ()
+	{
+		yield return new WaitForEndOfFrame ();
+		UnityEngine.VR.VRSettings.showDeviceView = false;
+	}
+}
