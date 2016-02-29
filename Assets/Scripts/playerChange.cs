d4503fd35a54c10e9b4b3af56534ba99e5c01bb5
Hasibe Erdemir <erdeh815@newschool.edu>
2016-02-29 18:22:13 -0500

    player change mechanic done :))))

---
+++ Assets/Scripts/playerChange.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class playerChange : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject model;
+	public float seconds;
+	public Text text;
+	public bool canTransform = true;
+
+
+	void Start () {
+		player1 = player1;
+		model = model;
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R) && canTransform == true) {
+			player1.SetActive (false);
+			model.SetActive (true);
+			InvokeRepeating ("Countdown", 1.0f, 1);
+			Invoke ("transformPlayer", 10);
+			canTransform = false; 
+		}
+	}
+	void Countdown(){
+		print (seconds);
+		seconds--; //Time.deltaTime;
+		text.text = "Time Left:" + Mathf.Round (seconds);
+	
+		if (seconds <= 0) {
+			CancelInvoke ("Countdown");
+			player1.SetActive (true);
+			model.SetActive (false);
+			seconds = 5;
+		}
+	}
+	void transformPlayer(){
+		canTransform = true;
+	}
+}
+
