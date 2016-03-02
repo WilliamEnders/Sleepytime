8e1c23ac26220a5a009588cea40a0d2f687a1406
Andre Cardoso <amc.acardoso@gmail.com>
2016-03-02 16:47:31 -0500

    Changes on raycast scene

---
+++ Assets/coneDetection.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coneDetection : MonoBehaviour {
+
+	public bool onSpot;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.Mouse0)) {
+			GetComponent<MeshCollider> ().enabled = true;
+			//TAKE THIS OFF LATER
+			GameObject.Find ("LIGHT").GetComponent<Light> ().enabled = true;
+		} else {
+			GetComponent<MeshCollider> ().enabled = false;
+			onSpot = false;
+			//TAKE THIS OFF LATER
+			GameObject.Find ("LIGHT").GetComponent<Light> ().enabled = false;
+		}
+
+
+	}
+
+	void OnTriggerStay (Collider other){
+		//print (other);
+		if (other.CompareTag ("Finish")) {
+			onSpot = true;
+		} 
+	}
+
+	void OnTriggerExit (Collider other) {
+		if (other.CompareTag ("Finish")) {
+			onSpot = false;
+		}
+	}
+		
+}
