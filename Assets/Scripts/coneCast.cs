2414024385beecaeb0d00c2f642110d743c50871
Andre Cardoso <amc.acardoso@gmail.com>
2016-03-03 16:31:07 -0500

    Raycast test scene complete (Empty game object lookat)

--- Assets/Scripts/coneCast.cs
+++ Assets/Scripts/coneCast.cs
@@ -13,6 +13,8 @@ public class coneCast : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		transform.LookAt (playerSpot);
 	
 	}
 
@@ -27,15 +29,18 @@ public class coneCast : MonoBehaviour {
 	void OnDrawGizmos() {
 		Gizmos.color = Color.red;
 		if (GameObject.Find ("cone").GetComponent<coneDetection> ().onSpot == true) {
-			Gizmos.DrawRay (transform.position, playerSpot.position * rayDistance);
+			Gizmos.DrawRay (transform.position, transform.forward * rayDistance);
 		}
 	}
 
 
 	void castRay() {
 		RaycastHit hitInfo;
-		if (Physics.Raycast (transform.position, playerSpot.position, out hitInfo)) {
-			Debug.Log ("HIT:" + hitInfo.collider.name);
+		if (Physics.Raycast (transform.position, transform.forward, out hitInfo)) {
+			if (hitInfo.collider.CompareTag ("Player")) {
+				//Debug.Log ("HIT:" + hitInfo.collider.name);
+				print ("i see you");
+			}
 		}
 	}
 
