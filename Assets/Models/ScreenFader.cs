9edaee55f977a3af80d89dbf218c77decf3bda52
William Enders <endew231@newschool.edu>
2016-03-15 20:33:19 -0400

    fade in fade out start screen text

---
+++ Assets/Models/ScreenFader.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ScreenFader : MonoBehaviour
+{
+	public Image FadeImg, FadeImg2;
+	public float fadeSpeed = 1.5f;
+	public bool sceneStarting = true;
+
+	void Update()
+	{
+		// If the scene is starting...
+		if (sceneStarting)
+			// ... call the StartScene function.
+			FadeToClear();
+	}
+
+
+	void FadeToClear()
+	{
+		// Lerp the colour of the image between itself and transparent.
+		FadeImg.color = Color.Lerp(FadeImg.color, Color.clear, fadeSpeed * Time.deltaTime);
+		FadeImg2.color = Color.Lerp(FadeImg2.color, Color.clear, fadeSpeed * Time.deltaTime);
+		if(FadeImg.color.a <= 0.05f){
+			FadeImg.color = Color.clear;
+			sceneStarting = false;
+			FadeImg.enabled = false;
+		}
+		if(FadeImg2.color.a <= 0.05f){
+			FadeImg2.color = Color.clear;
+			sceneStarting = false;
+			FadeImg2.enabled = false;
+		}
+	}
+
+	public void EndScene(int SceneNumber)
+	{
+		// Make sure the RawImage is enabled.
+		FadeImg.enabled = true;
+		FadeImg2.enabled = true;
+
+		FadeImg.color = Color.Lerp(FadeImg.color, Color.black, fadeSpeed * Time.deltaTime);
+		FadeImg2.color = Color.Lerp(FadeImg2.color, Color.black, fadeSpeed * Time.deltaTime);
+
+		// If the screen is almost black...
+		if (FadeImg.color.a >= 0.95f)
+			// ... reload the level
+			SceneManager.LoadScene(SceneNumber);
+	}
+
+}   
---
+++ Assets/lightGoesOut.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class lightGoesOut : MonoBehaviour {
+
+	private coneDetection cone;
+	private bool off;
+	private bool once;
+	public Text txt;
+
+	// Use this for initialization
+	void Start () {
+		once = false;
+		off = false;
+		cone = GameObject.Find ("cone").GetComponent<coneDetection> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(cone.energy <= 0){
+			off = true;
+		}
+		if(off && !once){
+			once = true;
+			txt.text += "\n Well, it's a little low on battery. Give it a second to charge.";
+		}
+	}
+}
